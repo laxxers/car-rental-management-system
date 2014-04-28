9de3c882b1f0bf5d4df691309e98aafe6ab85c8b
Chong Zhi Rui <laxxers@gmail.com>
2014-04-28 15:05:34 +0800

    Admin panel styling

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,8 +2,8 @@
 
 class Admin extends CI_Controller {
 	public function index() {
-		$this->load->view('header');
+		//$this->load->view('header');
 		$this->load->view('view_admin'); //Create a new view_admin file in views
-		$this->load->view('footer');
+		//this->load->view('footer');
 	}
 }
--- application/views/view_admin.php
+++ application/views/view_admin.php
@@ -1,51 +1,273 @@
-<h1>Admin</h1>
-<hr>
+<!DOCTYPE html>
+<html>
 
+<head>
 
-<div class="row">	
-	<div class="col-xs-12 col-md-3">
-		<ul class="nav nav-pills nav-stacked" >
-		  	<li><a href="#summary" data-toggle="tab" >Summary</a></li>
-		  	<li><a href="#vehicle" data-toggle="tab">Vehicles Inventory</a></li>
-		  	<li><a href="#package" data-toggle="tab">Packages & Charges </a></li>
-		  	<li><a href="#schedule" data-toggle="tab">Rental Schedule</a></li>
-		</ul>
-	</div>
-	
-	<div class="col-xs-12 col-md-9">
-		<div class="tab-content">
-		  	<div class="tab-pane " id="summary">
-				<ul class="list-group">
-					<li class="list-group-item">
-						<span class="badge">how many</span>
-						Total Admin Users
-					</li>
-					<li class="list-group-item">
-						<span class="badge">how many</span>
-						Total Registered Members
-					</li>
-					<li class="list-group-item">
-						<span class="badge">how many</span>
-						Total Available Vehicle to Rent
-					</li>
-					<li class="list-group-item">
-						<span class="badge">how many</span>
-						Total Rented Vehicle
-					</li>
-				</ul>
-			</div>
-			
-			<div class="tab-pane " id="vehicle">
-				<h3>coming soon1 ...</h3>
-			</div>
-			
-			<div class="tab-pane " id="package">
-				<h3>coming soon2 ...</h3>
-			</div>
-			
-			<div class="tab-pane " id="schedule">
-				<h3>coming soon3 ...</h3>
-			</div>
-		</div>
-	</div>
-</div>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Admin Panel</title>
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/css/admin_custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+                
+				<a href="#" class="btn btn-success navbar-btn navbar-right" style="margin-left: 25px;"><span class="glyphicon glyphicon-home"></span> Back to Home</a>		 
+            </div>
+            <!-- /.navbar-header -->
+            	
+            <div class="navbar-default navbar-static-side" role="navigation">
+
+                <div class="sidebar-collapse">
+                    <ul class="nav" id="side-menu">
+                    	<li>
+                    		<center>
+                    			<img src="pic/1/pic1.jpg" class="img-thumbnail" alt ="Profile Picture" width="200" style="margin: 20px;"/>
+	                    		<h3><strong>Nicole Charley</strong></h3>
+	                    		<h4><i>'admin'</i><h4>
+	                    			<br>
+                    		</center>
+                    	</li>
+                        <li>
+                            <a href="index.html"> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"> User Management</a>
+                        </li>
+                        <li>
+                            <a href="#"> Vehicle Inventory</a>
+                        </li>
+                        <li>
+                            <a href="#"> Package & Charges</a>
+                        </li>
+                        <li>
+                            <a href="#"> Rental Schedule</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Dashboard</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Registered Users' Information
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs">
+                                        <span class="glyphicon glyphicon-edit"></span> View All   
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="table-responsive">
+                        	<table class="table table-bordered table-hover table-striped">
+						    	<thead>
+									<tr>
+										<th>#</th>
+										<th>Username</th>
+										<th>Name</th>
+										<th>Email</th>
+										<th>Joined date</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1</td>
+										<td>admin</td>
+										<td>Nicole Minor</td>
+										<td>admin@example.com</td>
+										<td>2014-04-23</td>
+									</tr>                                              
+									<tr>
+										<td>2</td>
+										<td>user</td>
+										<td>Kirby Charley</td>
+										<td>user@example.com</td>
+										<td>2014-04-25</td>
+									</tr>                                              
+									<tr>
+										<td>3</td>
+										<td>moderator</td>
+										<td>Lina Laguna</td>
+										<td>lina@example.com</td>
+										<td>2014-04-24</td>
+									</tr>                                              
+									<tr>
+										<td>4</td>
+										<td>test</td>
+										<td>Rachel Moore</td>
+										<td>rachel@example.com</td>
+										<td>2014-04-26</td>
+									</tr>                                              
+								</tbody>
+						  	</table>
+						</div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                             Vehicle Inventory
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs">
+                                        <span class="glyphicon glyphicon-edit"></span> View All   
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                             
+							<div class="table-responsive">
+								<table class="table table-bordered table-hover table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Date</th>
+                                            <th>Time</th>
+                                            <th>Amount</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>3326</td>
+                                            <td>10/21/2013</td>
+                                            <td>3:29 PM</td>
+                                            <td>$321.33</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3325</td>
+                                            <td>10/21/2013</td>
+                                            <td>3:20 PM</td>
+                                            <td>$234.34</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3324</td>
+                                            <td>10/21/2013</td>
+                                            <td>3:03 PM</td>
+                                            <td>$724.17</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3323</td>
+                                            <td>10/21/2013</td>
+                                            <td>3:00 PM</td>
+                                            <td>$23.71</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3322</td>
+                                            <td>10/21/2013</td>
+                                            <td>2:49 PM</td>
+                                            <td>$8345.23</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3321</td>
+                                            <td>10/21/2013</td>
+                                            <td>2:23 PM</td>
+                                            <td>$245.12</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3320</td>
+                                            <td>10/21/2013</td>
+                                            <td>2:15 PM</td>
+                                            <td>$5663.54</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3319</td>
+                                            <td>10/21/2013</td>
+                                            <td>2:13 PM</td>
+                                            <td>$943.45</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Summary
+                        </div>
+ 
+                        <div class="panel-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item">
+                                    Admin Users
+                                    <span class="pull-right text-muted small"><em>1</em>
+                                    </span>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    Registered Members
+                                    <span class="pull-right text-muted small"><em>21</em>
+                                    </span>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    Available Vehicles
+                                    <span class="pull-right text-muted small"><em>23</em>
+                                    </span>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    Rented Vehicles
+                                    <span class="pull-right text-muted small"><em>45</em>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Active Admin Users
+                        </div>
+                        <div class="panel-body">
+                            <a href="#" class="btn btn-default btn-block">View Details</a>
+                        </div>
+                    </div>                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script>
+    	//Loads the correct sidebar on window load,
+		//collapses the sidebar on window resize.
+	    $(function() {
+	    $(window).bind("load resize", function() {
+	        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+	        if (width < 768) {
+	            $('div.sidebar-collapse').addClass('collapse')
+	        } else {
+	            $('div.sidebar-collapse').removeClass('collapse')
+	        }
+	    })
+		})
+    </script>
+
+</body>
+
+</html>
