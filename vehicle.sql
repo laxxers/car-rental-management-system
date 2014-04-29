7fea090025f6ff6267a3aeb5738ac81e77ebec7d
hauce93 <hau-hce@hotmail.com>
2014-04-29 20:15:11 +0800

    new table

--- vehicle.sql
+++ vehicle.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 29, 2014 at 07:07 AM
+-- Generation Time: Apr 29, 2014 at 12:14 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -33,21 +33,24 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
   `hourly` int(11) NOT NULL,
   `daily` int(11) NOT NULL,
   `weekly` int(11) NOT NULL,
-  `monthly` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `monthly` int(11) NOT NULL,
+  `no_passenger` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `ac` enum('1','0') NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `vehicle`
 --
 
-INSERT INTO `vehicle` (`type`, `name`, `details`, `hourly`, `daily`, `weekly`, `monthly`) VALUES
-('small', 'viva', 'auto', 6, 150, 900, 1500),
-('small', 'viva', 'manual', 6, 150, 900, 1500),
-('medium', 'saga', 'auto', 1000, 1000, 1000, 1000),
-('medium', 'vios', 'auto', 1000, 1000, 1000, 1000),
-('luxury', 'accord', 'auto', 1000, 1000, 1000, 1000),
-('MPV', 'avanza', 'auto', 1000, 1000, 1000, 1000),
-('van', 'HiAce', 'auto', 1000, 1000, 1000, 1000);
+INSERT INTO `vehicle` (`type`, `name`, `details`, `hourly`, `daily`, `weekly`, `monthly`, `no_passenger`, `capacity`, `ac`, `id`) VALUES
+('Compact', 'Viva', 'Auto', 6, 150, 900, 1500, 4, 2, '1', 1),
+('Standard', 'Vios', 'Auto', 1000, 1000, 1000, 1000, 5, 4, '1', 2),
+('Luxury', 'Accord', 'Auto', 1000, 1000, 1000, 1000, 5, 5, '1', 3),
+('MPV', 'Avanza', 'Auto', 1000, 1000, 1000, 1000, 8, 4, '1', 4),
+('Van', 'HiAce', 'Auto', 1000, 1000, 1000, 1000, 12, 6, '0', 5);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
