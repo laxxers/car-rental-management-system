87444e5368e0d503b86f7b00884183be758979fe
hauce93 <hau-hce@hotmail.com>
2014-05-04 02:27:24 +0800

    reservation

--- application/views/footer.php
+++ application/views/footer.php
@@ -10,6 +10,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
 	<script src="<?php echo base_url(); ?>public/js/bootstrap-datepicker.js"></script>
+  
 	<script>
 		$('.datepicker').datepicker();
 		
