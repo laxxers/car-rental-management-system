d96c0e933d23d17cffcd7d08c55044e0540e833d
Chong Zhi Rui <laxxers@gmail.com>
2014-05-07 21:19:23 +0800

    Homepage

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,11 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-	public function index($msg = NULL) {
-		$data['msg'] = $msg;
-		//$this->load->view('header');
-		$this->load->view('view_home', $data);
-		//$this->load->view('footer');
+	public function index() {
+		$this->load->view('view_home');
+
 	}
 
 	public function logout() {
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -7,14 +7,14 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Basic Company Template for Bootstrap 3</title>
+    <title>Car Rental Management System | Home</title>
 
     <!-- Bootstrap core CSS -->
 	<link href="<?php echo base_url(); ?>public/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>public/css/home_custom.css" rel="stylesheet">
 
-    <!-- Custom CSS for the 'Business Frontpage' Template -->
-    <link href="css/business-frontpage.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 </head>
 
 <body>
@@ -28,18 +28,13 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Start Bootstrap</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <div class="collapse navbar-collapse">
+
                 <ul class="nav navbar-nav">
-                    <li><a href="#about">About</a>
-                    </li>
-                    <li><a href="#services">Services</a>
-                    </li>
-                    <li><a href="#contact">Contact</a>
-                    </li>
+                    <li><a href="<?php echo base_url();?>"><i class="fa fa-home fa-lg"></i> Home</a></li>
                 </ul>
 
                 <ul class="nav navbar-nav navbar-right">
@@ -56,16 +51,39 @@
         <div class="container">
 
             <div class="row">
-                <div class="col-lg-8">
-                    <!-- The background image is set in the custom CSS -->
-                    <h1 class="tagline">Car Rental Management System</h1>
+                <div class="col-lg-12">
+                    <center>
+                        <h1 class="tagline">Car Rental Management System</h1>
+                        <p>Description</p>
+                    </center>
+                    
                 </div>
-                <div class="col-lg-4">
-					<button>Submit</button>
-
+            </div>
+             <hr>
+            <div class="row">
+                <div class="col-lg-8 col-sm-8" id="home-signup">
+                    <h2>What We Do</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel pellentesque quam. Ut dictum eu risus nec volutpat. Sed vitae dui lacus. Duis sed bibendum lorem. Morbi tempor elementum justo in tristique. Fusce sit amet lacus sed lacus tincidunt interdum. Curabitur tincidunt dolor nulla, a scelerisque orci tempor a. Duis eu quam bibendum, elementum turpis sit amet, blandit nibh. Sed et rhoncus tellus, sit amet dignissim mauris. Nunc suscipit tortor sed lobortis facilisis. Pellentesque risus metus, iaculis vitae justo nec, imperdiet aliquam libero. Duis tincidunt orci in feugiat scelerisque</p>
+                    <p><a class="btn btn-default btn-large" href="<?php echo base_url(); ?>register">Register an account &raquo;</a>
+                    </p>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <h2>Contact Us</h2>
+                    <address>
+                        <strong>Twitter, Inc.</strong>
+                        <br>795 Folsom Ave, Suite 600
+                        <br>San Francisco, CA 94107
+                        <br>
+                    </address>
+                    <address>
+                        <abbr title="Phone">P:</abbr>(123) 456-7890
+                        <br>
+                        <abbr title="Email">E:</abbr> <a href="mailto:#">first.last@example.com</a>
+                    </address>
                 </div>
             </div>
 
+        <hr>
         </div>
 
     </div>
@@ -74,55 +92,10 @@
 
         <hr>
 
-        <div class="row">
-            <div class="col-lg-8 col-sm-8">
-                <h2>What We Do</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel pellentesque quam. Ut dictum eu risus nec volutpat. Sed vitae dui lacus. Duis sed bibendum lorem. Morbi tempor elementum justo in tristique. Fusce sit amet lacus sed lacus tincidunt interdum. Curabitur tincidunt dolor nulla, a scelerisque orci tempor a. Duis eu quam bibendum, elementum turpis sit amet, blandit nibh. Sed et rhoncus tellus, sit amet dignissim mauris. Nunc suscipit tortor sed lobortis facilisis. Pellentesque risus metus, iaculis vitae justo nec, imperdiet aliquam libero. Duis tincidunt orci in feugiat scelerisque</p>
-                <p><a class="btn btn-default btn-large" href="#">Call to action &raquo;</a>
-                </p>
-            </div>
-            <div class="col-lg-4 col-sm-4">
-                <h2>Contact Us</h2>
-                <address>
-                    <strong>Twitter, Inc.</strong>
-                    <br>795 Folsom Ave, Suite 600
-                    <br>San Francisco, CA 94107
-                    <br>
-                </address>
-                <address>
-                    <abbr title="Phone">P:</abbr>(123) 456-7890
-                    <br>
-                    <abbr title="Email">E:</abbr> <a href="mailto:#">first.last@example.com</a>
-                </address>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col-sm-4">
-                <img class="img-circle img-responsive text-center" src="http://placehold.it/300x300">
-                <h2>Marketing Box #1</h2>
-                <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
-            </div>
-            <div class="col-sm-4">
-                <img class="img-circle img-responsive" src="http://placehold.it/300x300">
-                <h2>Marketing Box #2</h2>
-                <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-            </div>
-            <div class="col-sm-4">
-                <img class="img-circle img-responsive" src="http://placehold.it/300x300">
-                <h2>Marketing Box #3</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-            </div>
-        </div>
-
-        <hr>
-
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Company 2013</p>
+                    <p>&copy; Company 2014</p>
                 </div>
             </div>
         </footer>
