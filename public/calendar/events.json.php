72f4951a423f2713e301bce777a284feeb497593
hauce93 <hau-hce@hotmail.com>
2014-05-09 01:44:42 +0800

    schedule aded X php

--- public/calendar/events.json.php
+++ public/calendar/events.json.php
@@ -77,20 +77,15 @@
 }
 
 <?php
-// $db    = new PDO('mysql:host=localhost;
-				// dbname=car_rental;
-				// charset=utf8', 
-				// 'root',
-				// '');
-// $start = $_REQUEST['pickup'] / 1000;
-// $end   = $_REQUEST['dropoff'] / 1000;
-// $sql   = sprintf('SELECT * FROM reservation',
+// $db    = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', 'username', 'password');
+// $start = $_REQUEST['from'] / 1000;
+// $end   = $_REQUEST['to'] / 1000;
+// $sql   = sprintf('SELECT * FROM events WHERE `datetime` BETWEEN %s and %s',
     // $db->quote(date('Y-m-d', $start)), $db->quote(date('Y-m-d', $end)))
 
 // $out = array()
 // foreach($db->query($sql) as $row) {
     // $out[] = array(
-        // 'id' => $row->id
         // 'id' => $row->id,
         // 'title' => $row->name,
         // 'url' => Helper::url($row->id),
