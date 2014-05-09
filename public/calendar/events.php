ebea4e362417fcf6fc1ad062b1ef1fe98688094f
hauce93 <hau-hce@hotmail.com>
2014-05-10 00:08:54 +0800

    calander with color

--- public/calendar/events.php
+++ public/calendar/events.php
@@ -3,9 +3,17 @@ $con  = mysqli_connect("localhost","root","","car_rental");
 
 $result = mysqli_query($con,"SELECT * FROM reservation");
 
+$count = 0;
+$color = array('event-important',
+'event-info',
+'event-warning',
+'event-inverse',
+'event-success',
+'event-special'
+);
+
 while($row = mysqli_fetch_array($result))
 {
-	
 	$mysqldate1 = strtotime( $row['pickup'] );
 	$phpdate1 = date( 'd-m-Y', $mysqldate1 );
 	$ms_phpdate1 = 1000 * strtotime($phpdate1);
@@ -19,10 +27,17 @@ while($row = mysqli_fetch_array($result))
 		'id' =>  $row['res_id'],
 		'title' => "vehicle with id : " . $row['vehicle_id'],
 		// 'url'=> 'http://www.example.com/',
-		// 'class' => 'event-important',
+		// 'class' => $color[$count],
+		'class' => $color[$count],
 		'start' => $ms_phpdate1,
 		'end' => $ms_phpdate2
 	);
+	
+	$count++;
+	if($count % 5 == 0)
+		$count = 0;
+	
+	
 }
 
 echo json_encode(array('success' => 1, 'result' => $out));
